28a1ec4318183c41539a1719d45b347b3ee9ec34
Kisara Kodithuwakku <kisarakd@gmail.com>
2024-03-02 21:59:25 +0530

    small changes

--- core/Army.java
+++ core/Army.java
@@ -12,6 +12,11 @@ public class Army {
         armyCharacters = new HashMap<>();
     }
 
+    public void battleArmy(Army army) {
+
+
+    }
+
     public boolean hasCharacter(String category) {
         if (armyCharacters.containsKey(category)) {
             return true;
@@ -19,37 +24,48 @@ public class Army {
         return false;
     }
 
-    public String getCharacterName(String category) {
-        return armyCharacters.get(category).getName();
-    }
-
     public void addCharacter(Character character) {
         armyCharacters.put(character.getCategory(), character);
     }
 
+
+    // Print Methods
+
     public void printSimpleInfo() {
         for (String category : Character.getCharacterOrder()) {
             if (armyCharacters.containsKey(category)) {
-                System.out.println(category + ": " + armyCharacters.get(category).getName());
+                System.out.println(category + ": " + getCharacterName(category));
+            }
+        }
+    }
+    
+    public void printInfo() {
+        for (String category : Character.getCharacterOrder()) {
+            if (armyCharacters.containsKey(category)) {
+
+                System.out.println(category + ": " + getCharacterName(category));
             }
         }
     }
 
+    // Getters
+
+    public Character getCharacter(String category) {
+        return armyCharacters.get(category);
+    }
+
+    public String getCharacterName(String category) {
+        return getCharacter(category).getName();
+    }
+
     public String getInfoString() {
-        return "T";
-    }
-
-
-    public void display() {
-        System.out.println("Okay");
-    }
-
-    public void displayDetailed() {
-
-    }
-
-    public void battleArmy(Army army) {
-
+        String info = "";
+        for (String category : Character.getCharacterOrder()) {
+            if (armyCharacters.containsKey(category)) {
+                info += category + ": " + getCharacterName(category) + "  ";
+            }
+        }
+        return info;
     }
 
 }
